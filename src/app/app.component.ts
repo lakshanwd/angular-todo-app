b628fb5a86124d355f66e811a0edf12270c8a2cb
Supun Dissanayake <supunlakshandissanayake@gmail.com>
2018-04-02 00:02:14 +0530

    added todo creation and update todo list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import { ToDoItem } from './to-do-item';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
-  title = 'app';
+
+  todo = 'app';
+  todoItems: ToDoItem[] = [];
+
+  addToDo($evt) {
+    if (this.todo.length > 0) {
+      this.todoItems.push(new ToDoItem(0, this.todo));
+      this.todo = '';
+    }
+  }
+
+  removeToDo($evt) {
+    console.log($evt);
+  }
 }
--- src/app/to-do-item.ts
+++ src/app/to-do-item.ts
@@ -1,2 +1,9 @@
 export class ToDoItem {
+    id: number;
+    description: string;
+
+    constructor(id: number, description: string) {
+        this.id = id;
+        this.description = description;
+    }
 }
